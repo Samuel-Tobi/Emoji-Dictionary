e6fac4f0207866167a0905e56ece3ce2324c47cf
samuel-tobi <samueltobi52@gmail.com>
2022-09-13 00:42:04 +0100

    Completed

---
+++ src/components/App.js
@@ -0,0 +1,26 @@
+import emojipedia from "../emojimeaning";
+import Entry from "./Entry";
+
+function createEntry(emojiTerm) {
+  return (
+    <Entry
+      key={emojiTerm.id}
+      emoji={emojiTerm.emoji}
+      name={emojiTerm.name}
+      description={emojiTerm.meaning}
+    />
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+      <dl className="dictionary">{emojipedia.map(createEntry)}</dl>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/emojimeaning.js
@@ -0,0 +1,88 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "🥰",
+    name: "Smiling face with smiling eyes and three hearts",
+    meaning:
+      "You are in love, sitting on cloud number nine. The romantic emoji with the enamored expression of the face stands for affairs of the heart.",
+  },
+  {
+    id: 5,
+    emoji: "😘",
+    name: " Face Throwing a Kiss",
+    meaning:
+      "Smiley sends you a loving kiss that comes from the heart. Amicable, to express affection or with romantic intent, as an expression of love. May weaken a sarcastic response or be a reaction to a rude message. Symbol for flirting and showing gratitude.",
+  },
+  {
+    id: 6,
+    emoji: "😚",
+    name: "Kissing Face With Closed Eyes",
+    meaning:
+      "Cute face with closed eyes and rosy cheeks. As a thank you for a tip or a favor. Loving kiss to the closest friends, family or darling.",
+  },
+  {
+    id: 7,
+    emoji: "😋",
+    name: " Face Savouring Delicious Food",
+    meaning:
+      "Bon appetit! Smiley is licking the corner of its mouth with its tongue. Is hungry, just cooked something very delicious or eaten a whole dish.",
+  },
+  {
+    id: 8,
+    emoji: "🤨",
+    name: " Face With a Raised Eyebrow",
+    meaning:
+      "With the questioning look on the face, resembles the actor “The Rock“ (Dwayne Johnson). Can be used to express skepticism, disbelief or disapproval.",
+  },
+  {
+    id: 9,
+    emoji: "😒",
+    name: "Unamused Face",
+    meaning:
+      "The grumpy, sullen gaze expresses dissatisfaction. Is not enthusiastic about a thing and shows that. Expression of disinterest and disapproval.",
+  },
+  {
+    id: 10,
+    emoji: "🙁",
+    name: " Slightly frowning face",
+    meaning:
+      "„I’m sorry to hear that“ or „What you’re saying annoys me a little bit“. The frown can show rejection and anger or a person is surprised, anxious.",
+  },
+  {
+    id: 11,
+    emoji: "🥺",
+    name: "Face with begging eyes",
+    meaning:
+      "The big eyes are shining, devoted facial expression. The emoji is entreatingly begging for something: wants to borrow some money or the car. If you cannot get ahead by asking for something, puppy dog eyes are the last resort to get what you want.",
+  },
+  {
+    id: 11,
+    emoji: "🫂",
+    name: "People hugging",
+    meaning:
+      "We’ll always be there for one another! You could use some affection or as a gesture that you would like to hug the other person. A hug expresses physical closeness, affection, comfort and security.",
+  },
+];
+
+export default emojipedia;
